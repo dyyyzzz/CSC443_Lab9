e40f77a328e46a64ddcf788019f8767ebe934171
dyyyzzz <nuradilah688@gmail.com>
2022-06-21 23:29:27 +0800

    commit

--- next_page.php
+++ next_page.php
@@ -3,13 +3,13 @@
     echo "<html>";
 
     $queryString = $_SERVER["QUERY_STRING"];
-    echo "Query string of the incoming URL: "$queryString."\n';
+    echo "Query string of the incoming URL: ".$queryString."\n';
 
     echo "Cookies receieved:\n";
     foreach ($_COOKIE as $name => $value) {
             echo " $name = $value\n";
     }
-        
+
     $myLogin = $_SESSION ["myLogin"];
     echo "Value of myLogin has been retreived: ".$myLogin."\n";
 
